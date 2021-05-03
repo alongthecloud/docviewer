661a0b7ae1622dd67a6a12fd93616a7558ad4697
kim <alongthecloud@gmail.com>
2021-05-03 16:31:55 +0900

    settingsview 변경. 사용 라이브러리 변경.

--- lib/settingsview.dart
+++ lib/settingsview.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:settings_ui/settings_ui.dart';
+import 'package:flutter_settings/models/settings_list_item.dart';
+import 'package:flutter_settings/widgets/SettingsSection.dart';
+import 'package:flutter_settings/widgets/SettingsSelectionList.dart';
 
 class SettingsView extends StatelessWidget {
+  var fontItemList = <SettingsSelectionItem<int>>[
+    SettingsSelectionItem<int>(0, "System Default"),
+    SettingsSelectionItem<int>(1, "Nanum Gothic")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Settings')),
-        body: SafeArea(
-            child: SettingsList(
-          sections: [
-            SettingsSection(
-              title: 'Section',
-              tiles: [
-                SettingsTile(
-                  title: 'Language',
-                  subtitle: 'English',
-                  leading: Icon(Icons.language),
-                  onPressed: (BuildContext context) {},
-                ),
-              ],
-            ),
-          ],
-        )));
+      appBar: AppBar(title: Text('Settings')),
+      body: SafeArea(
+          child: ListView(
+        children: <Widget>[
+          SettingsSection(
+            title: Text('Appearance'),
+            settingsChildren: [
+              // ignore: missing_required_param
+              SettingsSelectionList<int>(
+                  context: context,
+                  items: fontItemList,
+                  title: 'Font',
+                  onSelect: (value, index) {
+                    debugPrint("You have selected " + value.text.toString());
+                  })
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
