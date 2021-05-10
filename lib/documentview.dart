59e1ba4c37bb649006404b635edce5e45eb1a011
kim <alongthecloud@gmail.com>
2021-05-11 00:40:53 +0900

    모바일에서는 flutter_webview_plugin 사용하여 렌더링.

--- lib/documentview.dart
+++ lib/documentview.dart
@@ -3,9 +3,10 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:path/path.dart' as path;
 import 'package:provider/provider.dart';
-
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'model/documentmodel.dart';
 
 class DocumentView extends StatefulWidget {
@@ -15,6 +16,7 @@ class DocumentView extends StatefulWidget {
 
 class _DocumentViewState extends State<DocumentView> {
   String _targetPath;
+  String _filename;
   String _htmlText = "";
 
   @override
@@ -25,24 +27,52 @@ class _DocumentViewState extends State<DocumentView> {
     var fileInfo = model.getSelectedFileInfo();
     Directory targetPath = model.targetPath;
     if (fileInfo != null) {
+      // _basePath = path.join(targetPath.path, fileInfo.folder);
       _targetPath =
           path.join(targetPath.path, fileInfo.folder, fileInfo.filename);
-
-      loadHtmlFile();
+      _filename = fileInfo.filename;
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if (Platform.isIOS || Platform.isAndroid) {
+      return buildWidgetMobile();
+    } else {
+      // Windows 등 Flutter 내장 위젯으로 HTML을 그린다.
+      return buildWidget();
+    }
+  }
+
+  Widget buildWidgetMobile() {
+    return WebviewScaffold(
+      url: Uri.file(_targetPath).toString(),
+      appBar: NewGradientAppBar(
+          title: Text(_filename),
+          gradient: LinearGradient(colors: [Colors.blue, Colors.lightBlue])),
+      withLocalStorage: true,
+      hidden: true,
+      initialChild: Container(
+          color: Colors.blueGrey[50],
+          child: const Center(
+            child: Text('.....'),
+          )),
+    );
+  }
+
+  Widget buildWidget() {
+    Widget htmlViewWidget;
+    if (_htmlText.isEmpty) {
+      loadHtmlFile();
+      htmlViewWidget = Text('Loading ...');
+    } else {
+      htmlViewWidget = HtmlWidget(_htmlText, baseUrl: Uri.file(_targetPath));
+    }
+
     return Scaffold(
-        appBar: AppBar(title: Text('Viewer')),
-        body: Container(
-            padding: EdgeInsets.all(18),
-            child: SingleChildScrollView(
-                child: HtmlWidget(
-              _getHtmlText(),
-              baseUrl: Uri.file(_targetPath),
-            ))));
+        appBar: AppBar(title: Text(_filename)),
+        body: SingleChildScrollView(
+            padding: EdgeInsets.all(10), child: htmlViewWidget));
   }
 
   void loadHtmlFile() async {
@@ -56,9 +86,5 @@ class _DocumentViewState extends State<DocumentView> {
     setState(() {
       _htmlText = txt;
     });
-  }
-
-  String _getHtmlText() {
-    return _htmlText;
   }
 }
